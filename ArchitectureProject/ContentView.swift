a1cc2b8a4bbb08dfe1b4a384705016f892ca7840
joao <joao.camargo@gmail.com>
2021-05-09 16:07:52 -0300

    beigin

---
+++ ArchitectureProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArchitectureProject
+//
+//  Created by joao camargo on 09/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
